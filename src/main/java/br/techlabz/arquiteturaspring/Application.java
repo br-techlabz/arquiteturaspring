c5c2de8f8668bef4a649cd9e9037a8d1ce5acb83
Willian <br.techlabz@proton.me>
2026-01-03 12:26:16 +0000

    Aula 35 e 36

--- src/main/java/br/techlabz/arquiteturaspring/Application.java
+++ src/main/java/br/techlabz/arquiteturaspring/Application.java
@@ -1,13 +1,22 @@
 package br.techlabz.arquiteturaspring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		//builder.lazyInitialization(true); // faz com que todos os beans sejm lazy, ou seja, não serao instanciados quando a aplicação for iniciada.
+
+		builder.run(args);
 	}
 
 }
--- src/main/java/br/techlabz/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/br/techlabz/arquiteturaspring/BeanGerenciado.java
@@ -3,9 +3,23 @@ package br.techlabz.arquiteturaspring;
 import br.techlabz.arquiteturaspring.todos.TodoEntity;
 import br.techlabz.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
+@Lazy // instancia o bean somente quando for utilizado. O padrao é NAO LAZY
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope("singleton") // Singleton => Escopo padrão. Uma instancia por toda a aplicação
+
+//@Scope(WebApplicationContext.SCOPE_REQUEST) ou
+//@Scope("request") // Request => Nao guarda estado ele existe somente na requisição. usado mais para aplicacoes web
+
+//@Scope("session") // Ativo por toda sessao do usuario
+
+//@Scope("application") // Ativo em toda aplicacao. usado aplicacoes web. mais abrangente que session e compartilhado por todos os usuarios
 public class BeanGerenciado {
 
     @Autowired // via propriedade
